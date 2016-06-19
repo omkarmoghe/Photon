107cf4b7e0be883dd68b87763c2fb7d907fdc99c
michaelm244 <michaelm244@gmail.com>
2016-06-18 19:37:31 -0700

    progress

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
-import flask
-import data
+import os, flask, data, random
 from pymongo import MongoClient
 app = flask.Flask(__name__)
+
+# Setting up uploads
+UPLOAD_FOLDER = 'images/'
+ALLOWED_EXTENSIONS = set(['jpeg'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mongo_address = 'mongodb://omkar:photos123@ds037175.mlab.com:37175/photos'
 client = MongoClient(mongo_address)
 db = client.photos
-coll = db.data
 
 @app.route('/')
 def hello_world():
@@ -35,6 +38,21 @@ def upload_contacts():
 def get_events():
     user_id = request.form['user_id']
     return flask.jsonify(data.get_user_events(user_id))
+
+
+@app.route('/upload_metadata', methods=['POST'])
+def upload_metadata():
+    metadata = request.form['metadata']
+
+    # See if there is a relevant event that this should be added to
+
+    return flask.jsonify({"success"}: True)
+
+@app.route('/upload_image', methods=[])
+def upload_image():
+    file = request.files['file']
+
+
 
 
 
