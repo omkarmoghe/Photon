b6b3c8c50dc7de91def9dfee6e238ef0d50de8d0
michaelm244 <michaelm244@gmail.com>
2016-06-18 17:19:20 -0700

    add

---
+++ data.py
@@ -0,0 +1,2 @@
+def get_user_events(user_id):
+    return []
--- server.py
+++ server.py
@@ -1,9 +1,31 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+import data
+app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/reg_user')
+def reg_user():
+    # Register user
+    user_id = 1
+    return flask.jsonify({
+        'user_id': user_id
+    })
+
+@app.route('/upload_contacts', methods=['POST'])
+def upload_contacts():
+    data = request.form
+    return flask.jsonify({
+        'success': True
+    })
+
+@app.route('/get_events', methods=['POST'])
+def get_events():
+    user_id = request.form['user_id']
+    return flask.jsonify(data.get_user_events(user_id))
+
 
 
 if __name__ == "__main__":
